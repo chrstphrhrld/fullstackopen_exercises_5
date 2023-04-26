710361bce07544a693082da3eb41cec7d7701bd2
chrstphrhrld <ch195@outlook.de>
2023-04-26 15:51:47 +0200

    extended state handling and additional components implemented

---
+++ frontend/src/components/NoteForm.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+const noteForm = ({ createNote }) => {
+
+	const [newNote, setNewNote] = useState('')
+
+	const addNote = (event) => {
+		event.preventDefault()
+		createNote({
+			content: newNote,
+			important: true
+		})
+
+		setNewNote('')
+	}
+
+	return (
+		<div>
+			<form onSubmit={ addNote }>
+				<input
+					value={ newNote }
+					onChange={ event => setNewNote(event.target.value) }
+				/>
+				<button type="submit">save</button>
+			</form>
+		</div>
+	)
+}
+
+export default noteForm
